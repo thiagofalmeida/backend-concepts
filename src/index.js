43fdacd0c8dffbd3bd130049e81d3408e82c4073
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-07-20 15:21:15 -0300

    Aplicação Funcional

--- src/index.js
+++ src/index.js
@@ -1,9 +1,65 @@
 const express = require('express');
+const { json } = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
+const projects = [];
 
-app.get('/', (request, response) => {
-    return response.json({name: 'Oi'});
+app.use(express.json());
+
+app.get('/projects', (request, response) => {
+    const { title } = request.query;
+
+    const results = title 
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
+
+    return response.json(results);
+});
+
+app.post('/projects', (request, response) => {
+    const { title, owner } = request.body;
+    
+    const project = { id:uuid(), title, owner };
+
+    projects.push(project);
+
+    return response.json(project);
+});
+
+app.put('/projects/:id', (request, response) => {
+    const { id } = request.params;
+    const { title, owner } = request.body;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if(projectIndex < 0) {
+        return response.status(400).json({ error: "Project not found!" });
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    }
+
+    projects[projectIndex] = project;
+
+    return response.json(project);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if(projectIndex < 0) {
+        return response.status(400).json({ error: "Project not found!" });
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send();
 });
 
 app.listen(3333, () => {
